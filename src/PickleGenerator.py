66a5bfc31ffd2eaf878db1c37f5b53e24367a61a
AndMastro <andrea.mastropietro@gmail.com>
2020-02-20 14:26:52 +0100

    Added datasets for all the 3 models

--- src/PickleGenerator.py
+++ src/PickleGenerator.py
@@ -9,25 +9,26 @@ import sys
 import pickle
 import csv
 from tqdm import tqdm
+from os import listdir
+from os.path import isfile, join
 
 
 
-DATASET_DIR_PATH = "../data/largeFiles/datasets/synthetic/"
-DATASET_NAME = "prova_EDM-1/prova_EDM-1_1.txt"
-DATASET_PATH = DATASET_DIR_PATH + DATASET_NAME
-PICKLE_PATH = "../data/largeFiles/datasets/pickles/prova.p"
+DATASET_DIR_PATH = "../data/largeFiles/datasets/synthetic/epistatic_risk_model_MAF02_eta01_theta2_EDM-1/"
+PICKLE_DIR_PATH = "../data/largeFiles/datasets/pickles/"
+
 
 
 '''this function is emplyed to generate pickles with the format one-hot
     from datasetas created  using GAMETES. We keep the predictive SNPs as the lst two ones, since
     our MLP does not care about order. If using a different architecture, this should be revised.'''
 
-def getDatasetFromGametes(saveMode=False):
+def getDatasetFromGametes(saveMode=False, datasetPath=None, picklePath=None):
     
     dataset = {}
     numIndividuals = 0
 
-    with open(DATASET_PATH, mode='r') as data_file:
+    with open(datasetPath, mode='r') as data_file:
         csv_reader = csv.reader(data_file, delimiter = "\t")
         next(csv_reader) #skip first line since it's header
 
@@ -51,10 +52,15 @@ def getDatasetFromGametes(saveMode=False):
 
     if saveMode == True:
         print("Saving pickle...")
-        with open(PICKLE_PATH, 'wb+') as handle:
+        with open(picklePath, 'wb+') as handle:
             pickle.dump(dataset, handle, protocol=pickle.HIGHEST_PROTOCOL)
         print("Saved.")
 
 if __name__ == "__main__":
-    getDatasetFromGametes(saveMode=True)
+    dataFiles = [f for f in listdir(DATASET_DIR_PATH) if isfile(join(DATASET_DIR_PATH, f))]
+    for d in tqdm(dataFiles):
+        fullPath = DATASET_DIR_PATH+d
+        pPath = PICKLE_DIR_PATH + d[0:-3] + "p"
+        getDatasetFromGametes(saveMode=True, datasetPath=fullPath, picklePath=pPath)
+    print("All datsets generated.")
     sys.exit()
