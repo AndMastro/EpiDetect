041fde109bdc674d21d23892fcc0515e5e6a1e4d
AndMastro <andrea.mastropietro@gmail.com>
2020-07-21 17:44:36 +0200

    Edit Cytoscape-like pipeline

--- src/AutoScape.py
+++ src/AutoScape.py
@@ -21,82 +21,58 @@ if __name__ == "__main__":
         for row in dataFile:
             line = row.strip().split("\t")
             edges.append((line[2], line[3]))
-            
-    G = nx.Graph()
+    
+    netName = DATA_FILE
+    G = nx.Graph(name = DATA_FILE)
     G.add_edges_from(edges)
 
-    nx.info(G)
+    print(nx.info(G))
 
     if centrality == "degree":
 
-        avg_degree = s = sum(dict(G.degree()).values())/G.number_of_nodes()
-        #print(avg_degree)
-
-
+        avg_degree = sum(dict(G.degree()).values())/G.number_of_nodes()
+        print("Average " + centrality + ": " + str(avg_degree))
 
         deg = G.degree()
-        #print(deg)
 
         deg_dict = {}
         for gene in deg:
             deg_dict[gene[0]] = gene[1]
 
-        print(deg_dict)
         # Create graph usong only central genes (deg > avg_degree)
 
 
         central_genes = []
-
+        central_measures = []
         for pair in deg:
             if pair[1] > avg_degree:
                 central_genes.append(pair[0])
-                
-        print(len(central_genes))
-
-
-
+                central_measures.append(pair[1])
+        
+        central_genes = [x for _,x in sorted(zip(central_measures,central_genes), reverse=True)]
         central_edges = []
 
         for gene1, gene2 in edges:
             if gene1 in central_genes and gene2 in central_genes:
                 central_edges.append((gene1, gene2))
-                
-        print(central_edges)
 
-
-
-
-
-        central_G = nx.Graph()
+        central_G = nx.Graph(name = "Central Genes Network")
 
         central_G.add_edges_from(central_edges)
 
-        nx.info(central_G)
+        print(nx.info(central_G))
+        
 
-
-
-
-
-        central_G.nodes()
-
-
-
-
-
-        SAVE_PATH = DATA_FILE[:-4] + "_CENTRAL_GENES.txt"
-        genes_list = list(central_G.nodes())
-
+        SAVE_PATH = DATA_FILE[:-4] + "_" + centrality +"_CENTRAL_GENES.txt"
+        
         with open(SAVE_PATH, "w+") as saveFile:
+            saveFile.write("Avg " + centrality +": " + str(avg_degree) + "\n\n")
             saveFile.write("Gene\tDegree\n")
-            for gene in genes_list:
+            for gene in central_genes:
                 saveFile.write(gene + "\t" + str(deg_dict[gene]) + "\n")
         
 
-
-
-
-
-        deg_list = sorted(list(G.degree()), reverse = True, key = lambda x:x[1])
+        '''deg_list = sorted(list(G.degree()), reverse = True, key = lambda x:x[1])
 
         central_deg_list = []
 
@@ -104,9 +80,98 @@ if __name__ == "__main__":
             if gene[0] in genes_list:
                 central_deg_list.append(gene)
                 
-        sorted(central_deg_list, reverse = True, key = lambda x:x[1])
-
+        print(sorted(central_deg_list, reverse = True, key = lambda x:x[1]))'''
+        
     else:
-        print("Centralioty measrue not recognized: choose from [degree, betweenness, TO ADD]")
-        sys.exit()
+        centrality_measure = None
+        avg_centrality = None
+
+        if centrality == "betweenness":
+
+            centrality_measure = nx.betweenness_centrality(G)
+            avg_centrality = sum(centrality_measure.values())/G.number_of_nodes()
+
+        elif centrality == "closeness":
+
+            centrality_measure = nx.closeness_centrality(G)
+            avg_centrality = sum(centrality_measure.values())/G.number_of_nodes()
+
+        elif centrality == "eigenvector":
+            centrality_measure = nx.eigenvector_centrality(G)
+            avg_centrality = sum(centrality_measure.values())/G.number_of_nodes()
+
+        elif centrality == "eccentricity":
+            centrality_measure = nx.eccentricity(G)
+            avg_centrality = sum(centrality_measure.values())/G.number_of_nodes()
+
+        elif centrality == "diameter":
+            diameter = nx.diameter(G)
+            print("\nNetwork diameter (max eccentricity): " + str(diameter))
+            sys.exit()
+
+        elif centrality == "average_distance":
+            distance = nx.average_shortest_path_length(G)
+            print("\nNetwork average distance (avg shortest path): " + str(distance))
+            sys.exit()
+        
+        elif centrality == "radiality":
+            print("\nNOT YET IMPLEMENTED")
+            sys.exit()
+        
+        elif centrality == "stress":
+            print("\nNOT YET IMPLEMENTED")
+            sys.exit()
+
+        elif centrality == "centroid_value":
+            print("\nNOT YET IMPLEMENTED")
+            sys.exit()
+
+        elif centrality == "bridging":
+            print("\nNOT YET IMPLEMENTED")
+            sys.exit()
+
+        elif centrality == "edge_betweenness":
+            print("\nNOT YET IMPLEMENTED")
+            sys.exit()
+            
+        else:
+            print("Centrality measure not implemented: choose from [degree, betweenness, closeness, eigenvector, eccentricity, diameter, average_distance, radiality, stress, centroid_value, bridging, edge_betweenness]")
+            sys.exit()
+
+        print("Average " + centrality + ": " + str(avg_centrality))
+
+        # Create graph usong only central genes (deg > avg_degree)
+
+
+        central_genes = []
+        central_measures = []
+        for gene in centrality_measure:
+            if centrality_measure[gene] > avg_centrality:
+                central_genes.append(gene)
+                central_measures.append(centrality_measure[gene])
+        
+        central_genes = [x for _,x in sorted(zip(central_measures,central_genes), reverse=True)]
+        central_edges = []
+        
+        for gene1, gene2 in edges:
+            if gene1 in central_genes and gene2 in central_genes:
+                central_edges.append((gene1, gene2))
+        central_G = nx.Graph(name = "Central Genes Network")
+
+        central_G.add_edges_from(central_edges)
+
+        print(nx.info(central_G))
+
+        central_G.nodes()
+
+        SAVE_PATH = DATA_FILE[:-4] + "_" + centrality +"_CENTRAL_GENES.txt"
+        
+
+        with open(SAVE_PATH, "w+") as saveFile:
+            saveFile.write("Avg " + centrality +": " + str(avg_centrality) + "\n\n")
+            saveFile.write("Gene\t"+ centrality +"\n")
+            for gene in central_genes:
+                saveFile.write(gene + "\t" + str(centrality_measure[gene]) + "\n")
+
+        
     # To add: take in input interaction file and centrality measure to use. Then, return a file with central genes: GENE_NAME DEG_ORIGINAL DEG_CENTRAL_NETWORK + avg degree in both networks (may be useful). To that for now, then, decide if consider edge weight.
